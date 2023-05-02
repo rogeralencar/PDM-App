72d5821fe9733e1e96dc27fda7a6a7e2a3e9fcb0
Gui1111RIbeiro <guilhermevilaferrara@gmail.com>
2023-05-02 14:24:34 -0300

    corrigindo bugs na tela de produtos

--- lib/src/common/components/product_grid_item.dart
+++ lib/src/common/components/product_grid_item.dart
@@ -1,32 +1,86 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'product_grid_item.dart';
+import '../models/cart.dart';
 import '../models/product.dart';
-import '../models/product_list.dart';
+import '../utils/app_routes.dart';
 
-class ProductGrid extends StatelessWidget {
-  final bool showFavoriteOnly;
-
-  const ProductGrid(this.showFavoriteOnly, {Key? key}) : super(key: key);
+class ProductGridItem extends StatelessWidget {
+  const ProductGridItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts =
-        showFavoriteOnly ? provider.favoriteItems : provider.items;
-    return GridView.builder(
-      padding: const EdgeInsets.all(10),
-      itemCount: loadedProducts.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-        value: loadedProducts[i],
-        child: ProductGridItem(),
-      ),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
+    final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
+    bool startsWithFile =
+        product.image.toString().toLowerCase().startsWith('https://');
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              onPressed: () {
+                product.toggleFavorite();
+              },
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+          title: Text(
+            product.name,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('Produto adicionado com sucesso!'),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'DESFAZER',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
+              cart.addItem(product);
+            },
+          ),
+        ),
+        child: GestureDetector(
+          child: Hero(
+            tag: product.id,
+            child: startsWithFile
+                ? FadeInImage(
+                    placeholder: const AssetImage(
+                        'assets/images/product-placeholder.png'),
+                    image: NetworkImage(product.image),
+                    fit: BoxFit.cover,
+                  )
+                : FadeInImage(
+                    placeholder: const AssetImage(
+                        'assets/images/product-placeholder.png'),
+                    image: FileImage(File(product.image)),
+                    fit: BoxFit.cover,
+                  ),
+          ),
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              AppRoutes.productsDetail,
+              arguments: product,
+            );
+          },
+        ),
       ),
     );
   }
