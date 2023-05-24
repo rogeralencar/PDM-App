a070acdbac107cca21bb9736016009549b1f3efd
Gui1111RIbeiro <guilhermevilaferrara@gmail.com>
2023-05-24 13:49:23 -0300

    Criando nova tela de home - desenvolvendo ainda

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -21,7 +21,202 @@ import 'feature/home/view/page/products_overview_screen.dart';
 import 'feature/onboarding/view/page/onboarding_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Teste());
+}
+
+class Teste extends StatefulWidget {
+  const Teste({Key? key}) : super(key: key);
+
+  @override
+  TesteState createState() => TesteState();
+}
+
+class TesteState extends State<Teste> {
+  int _currentPage = 0;
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> indicators = [];
+    for (int i = 0; i < 3; i++) {
+      indicators.add(
+        i == _currentPage ? _indicator(true) : _indicator(false),
+      );
+    }
+    return indicators;
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 150),
+      margin: const EdgeInsets.symmetric(horizontal: 2.0),
+      height: 4.0,
+      width: 20,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.black : Colors.grey.shade300,
+        borderRadius: const BorderRadius.all(Radius.circular(4)),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Material(
+                    elevation: 4,
+                    shape: const CircleBorder(),
+                    child: Ink(
+                      decoration: const ShapeDecoration(
+                        color: Colors.transparent,
+                        shape: CircleBorder(),
+                      ),
+                      child: InkWell(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        onTap: () {
+                          // Implemente a funcionalidade do botão de perfil aqui
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(Icons.person_outlined),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Material(
+                    elevation: 4,
+                    shape: const CircleBorder(),
+                    child: Ink(
+                      decoration: const ShapeDecoration(
+                        color: Colors.transparent,
+                        shape: CircleBorder(),
+                      ),
+                      child: InkWell(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        onTap: () {
+                          // Implemente a funcionalidade do botão de perfil aqui
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(Icons.shopping_cart_outlined),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Material(
+                elevation: 4,
+                borderRadius: BorderRadius.circular(18.0),
+                child: TextField(
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    suffixIcon: const Icon(
+                      Icons.search,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    // Implement search functionality here
+                  },
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.all(16),
+              height: 190.0,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: PageView.builder(
+                controller: _pageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentPage = index;
+                  });
+                },
+                itemCount:
+                    3, // Coloque o número total de itens do carrossel aqui
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      // Implemente a funcionalidade do carrossel aqui
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Image.network(
+                        'https://casaeconstrucao.org/wp-content/uploads/2022/07/2-modelo-de-frase-para-loja-de-roupas.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _buildPageIndicator(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Product Categories',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            // Add your product category widgets here
+            const SizedBox(height: 20.0),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Product Recommendations',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            // Add your product recommendation widgets here
+          ],
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
